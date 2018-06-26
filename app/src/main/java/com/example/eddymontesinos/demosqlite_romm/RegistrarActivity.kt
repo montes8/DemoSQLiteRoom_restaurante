874de262f12f63cf711994edba5d7709e9e68470
emontesinos812 <emontesinos812@gmail.com>
2018-06-26 08:37:51 -0300

    diseñanado activity_registrar

--- app/src/main/java/com/example/eddymontesinos/demosqlite_romm/RegistrarActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/RegistrarActivity.kt
@@ -2,11 +2,24 @@ package com.example.eddymontesinos.demosqlite_romm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registrar.*
 
 class RegistrarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar)
+
+        ajusteToolbar()
+    }
+
+
+    private fun ajusteToolbar() {
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.toolbar_registrate)
+        mToolbar.navigationIcon = getDrawable(R.drawable.ic_flecha_back)
+        mToolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
