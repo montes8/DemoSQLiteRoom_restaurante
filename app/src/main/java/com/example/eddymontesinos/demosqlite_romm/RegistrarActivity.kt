8443a82743bbb150dea3ebdb2209d146a767c559
emontesinos812 <emontesinos812@gmail.com>
2018-06-26 07:32:31 -0300

    creando Splash

---
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/RegistrarActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eddymontesinos.demosqlite_romm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar)
+    }
+}
