66a05b69a790b934a23f0e363be572d69a6f456a
emontesinos812 <emontesinos812@gmail.com>
2018-06-26 09:50:05 -0300

    validando campos del registro_activity , no esten vacios

--- app/src/main/java/com/example/eddymontesinos/demosqlite_romm/RegistrarActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/RegistrarActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.util.Patterns
 import com.example.eddymontesinos.demosqlite_romm.model.Usuario
 import kotlinx.android.synthetic.main.activity_registrar.*
 import org.jetbrains.anko.*
@@ -17,7 +18,6 @@ class RegistrarActivity : AppCompatActivity() {
         setContentView(R.layout.activity_registrar)
 
         ajusteToolbar()
-
         registrarUsuario()
     }
 
@@ -35,34 +35,57 @@ class RegistrarActivity : AppCompatActivity() {
         button_click_register.setOnClickListener {
 
 
-            Thread {
-                val usuario = Usuario()
-                usuario.nombre = edit_text_nombre.text.toString()
-                usuario.nombreUsuario = edit_text_user.text.toString()
-                usuario.contrasenia = edit_password.text.toString()
-                usuario.pais = edit_pais.text.toString()
 
-                val nuevoId = DemoApplication.database!!.usuarioDao().insert(usuario)
-                //val a:Int = resultado.toInt()
-                if (nuevoId > 0) {
-                    Log.i("idregistrado", "$nuevoId")
-                    handler.post {
-                        toast("Usuario Registrado")
-                        edit_text_nombre.setText("")
-                        edit_text_user.setText("")
-                        edit_password.setText("")
-                        edit_pais.setText("")
 
-                        defaultSharedPreferences.edit().putString("idUsuarioLogeado", nuevoId.toString()).apply()
 
-                        startActivity(intentFor<HomeActivity>().newTask().clearTask())
+                Thread {
+
+                    if (edit_text_nombre.text.toString().isEmpty()) {
+
+                        handler.post { toast("ingrese nombre") }
+
+                    }else if(edit_text_user.text.toString().isEmpty()){
+                        handler.post { toast("Ingrese nombre de usuario") }
+
+                    }else if(edit_password.text.toString().isEmpty()){
+                        handler.post { toast("Ingrese contraseña") }
+
+                    }else if(edit_pais.text.toString().isEmpty()){
+                        handler.post { toast("Ingrese pais") }
+                    }else {
+                        val usuario = Usuario()
+                        usuario.nombre = edit_text_nombre.text.toString()
+                        usuario.nombreUsuario = edit_text_user.text.toString()
+                        usuario.contrasenia = edit_password.text.toString()
+                        usuario.pais = edit_pais.text.toString()
+
+                        val nuevoId = DemoApplication.database!!.usuarioDao().insert(usuario)
+                        //val a:Int = resultado.toInt()
+                        if (nuevoId > 0) {
+                            Log.i("idregistrado", "$nuevoId")
+                            handler.post {
+                                toast("Usuario Registrado")
+                                edit_text_nombre.setText("")
+                                edit_text_user.setText("")
+                                edit_password.setText("")
+                                edit_pais.setText("")
+
+                                defaultSharedPreferences.edit().putString("idUsuarioLogeado", nuevoId.toString()).apply()
+
+                                startActivity(intentFor<HomeActivity>().newTask().clearTask())
+                            }
+                        } else {
+                            handler.post { toast("errorrrr") }
+                        }
                     }
-                } else {
-                    handler.post { toast("errorrrr") }
-                }
-            }.start()
-
+                }.start()
 
         }
+    }
+
+
+    fun validarnombre(email: String): Boolean {
+        //return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        return !email.isNullOrEmpty()
     }
 }
