cb04adf4c64f9a04dd3acc2404df51f748c1683c
montes8 <emontesinos812@gmail.com>
2018-06-28 19:54:14 -0500

    agregando menu

---
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/views/DetallePedido.kt
@@ -0,0 +1,13 @@
+package com.example.eddymontesinos.demosqlite_romm.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eddymontesinos.demosqlite_romm.R
+
+class DetallePedido : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pedido)
+    }
+}
