c2afaa85fefa8ff175c2b637b1ab3861498a0bcd
emontesinos812 <emontesinos812@gmail.com>
2018-07-02 12:59:42 -0300

    implementado diseño molde detalle

--- app/src/main/java/com/example/eddymontesinos/demosqlite_romm/adapter/DetalleAdapter.kt
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/adapter/DetalleAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.eddymontesinos.demosqlite_romm.adapter
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eddymontesinos.demosqlite_romm.R
 import com.example.eddymontesinos.demosqlite_romm.model.DetalleTemporal
+import com.example.eddymontesinos.demosqlite_romm.utils.DemoUtils
 import kotlinx.android.synthetic.main.molde_detalle_pedido.view.*
 
-class DetalleAdapter : RecyclerView.Adapter<DetalleAdapter.DetalleTemporalViewHolder>(){
+class DetalleAdapter(val contexto: Context) : RecyclerView.Adapter<DetalleAdapter.DetalleTemporalViewHolder>(){
 
     private var detalleaTemporal : List<DetalleTemporal>? = null
 
@@ -31,15 +33,22 @@ class DetalleAdapter : RecyclerView.Adapter<DetalleAdapter.DetalleTemporalViewHo
     override fun onBindViewHolder(holder: DetalleTemporalViewHolder, position: Int) {
         val detallePedido = detalleaTemporal!![position]
 
-        holder.idPlatoDetalle.text = detallePedido.plato.idPlato.toString()
+        val subtotal = detallePedido.plato.precioPlato * detallePedido.cantidad
+
+       holder.imagenDetalle.setImageDrawable(DemoUtils.getImage(contexto, detallePedido.plato.imagen))
+        holder.idPlatoDetalleNombre.text = detallePedido.plato.nombrePlato
+        holder.precioPlatounidad.text = detallePedido.plato.precioPlato.toString()
         holder.catidad.text = detallePedido.cantidad.toString()
-        holder.subtotal.text = detallePedido.plato.precioPlato.toString()
+        holder.subtotal.text = subtotal.toString()
     }
 
     class DetalleTemporalViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
 
-        val idPlatoDetalle = itemView.text_id_platodetalle
+
+        val imagenDetalle = itemView.image_view_plato_detalle
+        val idPlatoDetalleNombre = itemView.text_id_platodetalle
+        val precioPlatounidad = itemView.text_precio_unitario
         val catidad = itemView.text_cantidad_platos
-        val subtotal = itemView.text_precio_subtotal
+        val subtotal = itemView.text_subtotal_detalle
     }
 }
--- app/src/main/java/com/example/eddymontesinos/demosqlite_romm/views/DetallePedidoActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/views/DetallePedidoActivity.kt
@@ -1,6 +1,6 @@
 package com.example.eddymontesinos.demosqlite_romm.views
 
-import android.content.Intent
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -9,11 +9,8 @@ import android.view.Menu
 import android.view.MenuItem
 import com.example.eddymontesinos.demosqlite_romm.R
 import com.example.eddymontesinos.demosqlite_romm.adapter.DetalleAdapter
-import com.example.eddymontesinos.demosqlite_romm.model.DetalleTemporal
 import com.example.eddymontesinos.demosqlite_romm.repository.temporal.OrdenTemporal
 import kotlinx.android.synthetic.main.activity_detalle_pedido.*
-import kotlinx.android.synthetic.main.activity_detalle_plato.*
-import kotlinx.android.synthetic.main.activity_home.*
 import org.jetbrains.anko.toast
 
 class DetallePedidoActivity : AppCompatActivity() {
@@ -23,25 +20,20 @@ class DetallePedidoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_pedido)
-        setSupportActionBar(pedidoToolbar)
-        title = ""
-        pedidoToolbar.navigationIcon = getDrawable(R.drawable.ic_atras)
-        pedidoToolbar.setNavigationOnClickListener {
-            onBackPressed()}
-
-        detallesAdarper = DetalleAdapter()
-
-        //pedidosdetalle.add(DetalleTemporal(1,1,10.00))
-
-        //detallesAdarper?.addList(pedidosdetalle)
-        pedido_recyclerview.layoutManager = LinearLayoutManager(this)
-        pedido_recyclerview.adapter = detallesAdarper
-
+        ajustarToolbardetalle()
+        detallesAdarper = DetalleAdapter(this)
         val orden = OrdenTemporal.obtenerOrden()
 
         orden.forEach {
             Log.v("detalleorden", it.toString())
         }
+        //pedidosdetalle.add(DetalleTemporal(1,1,10.00))
+
+        detallesAdarper?.addList(orden)
+        pedido_recyclerview.layoutManager = LinearLayoutManager(this)
+        pedido_recyclerview.adapter = detallesAdarper
+
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -60,6 +52,16 @@ class DetallePedidoActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_orden, menu)
         return true
+    }
+
+
+    private fun ajustarToolbardetalle(){
+
+        setSupportActionBar(pedidoToolbar)
+        title = ""
+        pedidoToolbar.navigationIcon = getDrawable(R.drawable.ic_atras)
+        pedidoToolbar.setNavigationOnClickListener {
+            onBackPressed()}
     }
 
 }
