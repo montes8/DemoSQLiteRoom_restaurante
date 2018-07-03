71431f9fc9aabb3c5ed975556c0d852b0f8a8354
emontesinos812 <emontesinos812@gmail.com>
2018-07-03 11:52:13 -0300

    creando molde item historial

--- app/src/main/java/com/example/eddymontesinos/demosqlite_romm/views/HistorialActivity.kt
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/views/HistorialActivity.kt
@@ -2,11 +2,14 @@ package com.example.eddymontesinos.demosqlite_romm.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eddymontesinos.demosqlite_romm.R
 
 class HistorialActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_historial)
+
+
     }
 }
