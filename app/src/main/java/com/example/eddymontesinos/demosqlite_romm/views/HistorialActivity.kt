15c6798c1193fd7d31ada9c5f2ba7a80b5e5d723
emontesinos812 <emontesinos812@gmail.com>
2018-07-03 11:36:43 -0300

    creando vista historial pedidos

---
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/views/HistorialActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eddymontesinos.demosqlite_romm.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HistorialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historial)
+    }
+}
