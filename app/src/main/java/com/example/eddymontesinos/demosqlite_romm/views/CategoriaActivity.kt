aae8651abc9b786f54b1d85ab0e6f3b0690c898c
montes8 <emontesinos812@gmail.com>
2018-09-11 18:27:01 -0500

    creando nueva vista

---
+++ app/src/main/java/com/example/eddymontesinos/demosqlite_romm/views/CategoriaActivity.kt
@@ -0,0 +1,13 @@
+package com.example.eddymontesinos.demosqlite_romm.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eddymontesinos.demosqlite_romm.R
+
+class CategoriaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categoria)
+    }
+}
